47f9b4a3ffa9972cb384321e53dd2dffccbdf179
LINKER <temp2temp@daum.net>
2022-06-26 22:58:36 +0900

    inline style2

--- react-study/src/Hello.jsx
+++ react-study/src/Hello.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 function Hello() {
-  // inline style
-  return <div style={{marginTop:"20px", backgroundColor:"blue"}}>Hello World</div>;
+  const PracticeStyle ={
+    marginTop: "20px", 
+    backgroundColor: "red",
+  }
+  return( 
+    // inline style 2
+  <>
+  <div style={PracticeStyle}>Hello World</div>
+  <div style={{marginTop:"20px", backgroundColor:"blue"}}>Hello World</div>
+  <div style={{marginTop:"20px", backgroundColor:"blue"}}>Hello World</div>
+  </>
+  )
 }
 
 export default Hello;
