6a07bd82374b88bcf0fad72c69940ab3b148a6f9
LINKER <temp2temp@daum.net>
2022-06-26 23:33:43 +0900

    styled-components example

--- react-study/src/Hello.jsx
+++ react-study/src/Hello.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Hello.css";
+// styled-components import 
+import styled from "styled-components";
+
 function Hello() {
-  const PracticeStyle ={
-    marginTop: "20px", 
-    backgroundColor: "red",
-  }
+   // styled-components 사용
+   const StyledButton = styled.button`
+    color:red;
+    background-color:blue;
+    `;
+
   return( 
     // 외부파일 css
   <>
   <div className="red">Hello World</div>
   <div class="red">Hello World</div>
   <div className="red">Hello World</div>
+  <StyledButton>버튼예제</StyledButton>
   </>
   )
 }
